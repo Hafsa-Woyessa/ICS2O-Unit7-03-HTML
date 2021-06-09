4c88a462b5489e45c450d6d96b33d47cd1dba60d
HafsaWoyessa <84517071+Hafsa-Woyessa@users.noreply.github.com>
2021-06-09 14:25:48 +0000

    line 12

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let age = 0
 
 function comparision () {
 // get the age number and convert to an integer
- age = document.getElementById('input').value
- age = parseInt(age)
+  age = document.getElementById('input').value
+  age = parseInt(age)
 
   // checks if user's age  is younger than 17
   if (age >= 17) {
-  document.getElementById('answer').innerHTML = 'You can see an R  movie alone'
+    document.getElementById('answer').innerHTML = 'You can see an R  movie alone'
 } else if (age >= 13) {
-  document.getElementById('answer').innerHTML = 'You can see a PG-13 movie alone'
-} else if (age >= 5) {
-  document.getElementById('answer').innerHTML = 'You can see a G or PG  movie alone'
+   document.getElementById('answer').innerHTML = 'You can see a PG-13 movie alone'
+}  else if (age >= 5) {
+    document.getElementById('answer').innerHTML = 'You can see a G or PG  movie alone'
 } else  {
-  document.getElementById ('answer').innerHTML = 'You\'re too young to watch movies'
-}
+    document.getElementById('answer').innerHTML = 'You\'re too young to watch movies'
+  }
 }
